4204ed9d708fd64797d5f3020289e0c7608a30a8
XCrasseltX <22590978+XCrasseltX@users.noreply.github.com>
2026-04-28 14:14:01 +0200

    Für Github Pages aufbereitet

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Ermöglicht den statischen HTML-Export
+  images: {
+    unoptimized: true, // Wichtig, da GitHub Pages keine Bildoptimierung auf dem Server unterstützt
+  },
   experimental: {
     typedRoutes: true,
   },
 };
+
+basePath: '/crasselt-spacetech',
 
 export default nextConfig;
